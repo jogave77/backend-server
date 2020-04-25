3ce1f156b0d0ef47af46693d0d57af9d98b09ff7
jogave <jogave@hotmail.com>
2020-04-25 13:49:50 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+
+
+
+
+// Inicializar variables
+var app = express();
+
+//conexion a la bvase de datos
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'OnLine');
+
+});
+
+//Rutas
+app.get('/', (req, res, next) => {
+
+    res.status(403).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente.'
+    });
+
+});
+
+
+
+//Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'OnLine');
+});
